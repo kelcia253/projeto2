2679ea56fa0be583883a271fbb1045f959c4785e
kelcia253 <110485657+kelcia253@users.noreply.github.com>
2023-03-07 15:04:04 -0300

    arrumadinho

--- src/app/paginas/novo-componente/novo-componente.component.ts
+++ src/app/paginas/novo-componente/novo-componente.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-novo-componente',
   templateUrl: './novo-componente.component.html',
   styleUrls: ['./novo-componente.component.css']
 })
-export class NovoComponenteComponent {
+export class NovoComponenteComponent implements OnInit {
+  private nome:string;
+  anoNascimento: number;
+
+  constructor(){
+    this.nome = "Fulano ";
+    this.anoNascimento = 2005;
+
+  }
+  idade ():number{
+
+    return 2023 - this.anoNascimento;
+  }
+  ngOnInit(): void {
+    console.log("nome: "+ this.nome);
+    console.log("idade: " + this.idade());
+  }
 
 }
