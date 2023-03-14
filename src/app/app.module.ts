08d0a496f30290f6b58a12161d75d08468487a97
kelcia253 <110485657+kelcia253@users.noreply.github.com>
2023-03-14 14:54:43 -0300

    14/03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
 import { NovoComponenteComponent } from './paginas/novo-componente/novo-componente.component';
@@ -10,7 +12,8 @@ import { NovoComponenteComponent } from './paginas/novo-componente/novo-componen
     NovoComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
